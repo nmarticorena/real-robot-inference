b09bc0843751bb75657c677045f30ea0112a1da3
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2025-11-14 10:35:05 +1000

    Add some handy scripts

--- scripts/frankx/get_joints.py
+++ scripts/frankx/get_joints.py
@@ -3,3 +3,4 @@ from frankx import Robot
 robot  =  Robot("172.16.0.2", repeat_on_error= True, user = "franka", password="franka123")
 breakpoint()
 print(robot.current_joint_positions())
+
---
+++ scripts/pandapy/get_delta.py
@@ -0,0 +1,18 @@
+import panda_py 
+import frankx
+import spatialmath as sm
+import numpy as np
+from roboticstoolbox.models import Panda
+panda = Panda()
+
+for i in range(4):
+    q = panda.random_q()
+    T_0p = sm.SE3(panda_py.fk(q), check = False)
+    T_0f = sm.SE3(np.array(frankx.Kinematics.forward(q)).reshape(4,4, order = "F"))
+
+    T_pf = T_0p.inv() * T_0f
+    T_fp = T_0f.inv() * T_0p
+    print(T_pf)
+    print(T_fp)
+
+
