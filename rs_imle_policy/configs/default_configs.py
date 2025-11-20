b2659ae5816fa0184456edcdcb5507d451aec59e
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2025-11-20 18:02:29 +1000

    add default configs

---
+++ rs_imle_policy/configs/default_configs.py
@@ -0,0 +1,12 @@
+from rs_imle_policy.configs.train_config import TrainConfig, RSIMLE, Diffusion
+from dataclasses import dataclass, field
+
+
+@dataclass
+class PickPlaceRSMLEConfig(TrainConfig):
+    model: RSIMLE | Diffusion = field(
+        default_factory=lambda: RSIMLE(
+            lowdim_obs_keys=("robot_pos", "robot_orien", "gripper_state"),
+            action_keys=("action_pos", "action_orien", "action_gripper"),
+        )
+    )
