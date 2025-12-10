af25b6178d27690fe7139d7930a5aa25595f1b2a
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2025-12-11 09:55:18 +1000

    attempt to visualize the relative pos approach

--- rs_imle_policy/dataset_utils/replay_demonstrations.py
+++ rs_imle_policy/dataset_utils/replay_demonstrations.py
@@ -1,11 +1,9 @@
 from rs_imle_policy.visualizer.rtb import RobotViz
-import spatialmath as sm
 import numpy as np
 from rs_imle_policy.dataset import PolicyDataset, unnormalize_data
 from rs_imle_policy.configs.train_config import LoaderConfig, VisionConfig
 import rs_imle_policy.utilities as utils
 import cv2
-import torch
 import spatialgeometry as sg
 
 import tyro
@@ -30,6 +28,12 @@ env.env.add(robot_pos)
 action_pos = sg.Axes(0.05)
 env.env.add(action_pos)
 
+arrow = sg.Arrow(0.1, color=(1, 0, 0))
+env.env.add(arrow)
+arrow.attach_to(robot_pos)
+
+action_pose_2 = sg.Axes(0.025, color=(0, 1, 0))
+env.env.add(action_pose_2)
 
 for episode in rlds:
     ep_data = rlds[episode]
@@ -45,28 +49,30 @@ for episode in rlds:
         X_BE_gello = env.robot.fkine(np.array(gello_q), "panda_link8")
 
         # create a pose at 0,0,0
-        pose = sm.SE3(0, 0, 0)
         orien = unnormalize_data(
             ep_data["action_orien"][idx], dataset.stats["action_orien"]
         )
-        R = utils.rotation_6d_to_matrix(torch.from_numpy(orien))
         t = unnormalize_data(ep_data["action_pos"][idx], dataset.stats["action_pos"])
-        #
-        pose.R = sm.SO3(R.numpy(), check=False).norm()
-        pose.t = t
-        action_pos.T = pose
+
+        # pose = utils.pos_rot_to_se3(t, orien)
+        # action_pos.T = pose
 
         orien = unnormalize_data(
             ep_data["robot_orien"][idx], dataset.stats["robot_orien"]
         )
-        R = utils.rotation_6d_to_matrix(torch.from_numpy(orien))
         t = unnormalize_data(ep_data["robot_pos"][idx], dataset.stats["robot_pos"])
-        r_pos = sm.SE3()
-
-        r_pos.R = sm.SO3(R.numpy(), check=False).norm()
-        r_pos.t = t
-
+        r_pos = utils.pos_rot_to_se3(t, orien)
         robot_pos.T = r_pos
+
+        arrow_length = unnormalize_data(
+            ep_data["relative_pos"][idx], dataset.stats["relative_pos"]
+        )
+        arrow_dir = unnormalize_data(
+            ep_data["relative_orien"][idx], dataset.stats["relative_orien"]
+        )
+
+        pose_2 = utils.pos_rot_to_se3(arrow_length, arrow_dir)
+        action_pose_2.T = r_pos * pose_2
 
         env.step(robot_q, gello_q)
 
@@ -80,5 +86,3 @@ for episode in rlds:
         # show one combined window
         cv2.imshow("viewer", merged)
         cv2.waitKey(1)
-
-        # time.sleep(0.001)
