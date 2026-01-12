d7fb68963d088c2b9407c010f102493657458645
nmarticorena <nicolas.marticorena.vidal@gmail.com>
2026-01-12 14:08:15 +1000

    re-organize rerun tools

--- rs_imle_policy/visualizer/rerun_tools.py
+++ rs_imle_policy/visualizer/rerun_tools.py
@@ -1,5 +1,4 @@
 from numpy.typing import NDArray
-import av
 from typing import Optional
 import rerun as rr
 import spatialgeometry as sg
@@ -7,39 +6,82 @@ import trimesh
 import roboticstoolbox as rtb
 import numpy as np
 
-from rs_imle_policy.configs.train_config import VisionConfig
-
 
 class ReRunRobot:
-    def __init__(self, robot: rtb.Robot, leader=False):
+    def __init__(
+        self, robot: rtb.Robot, prefix: str, alpha: Optional[float] = None
+    ) -> None:
         self.robot = robot
-        self.robot_name = robot.name
-        self.leader_name = robot.name + "_leader"
-        self.leader = leader
-        self.load_meshes()
+        self.name = f"{prefix}/{robot.name}"
+        self.alpha = alpha
+        self._load_meshes()
 
-    def log_pose(self, position, quaternion, name="pose"):
-        # Pytorch 3D and rerun use a different order
-        quad = [quaternion[1], quaternion[2], quaternion[3], quaternion[0]]
+    def log_robot_state(self, q: NDArray) -> None:
+        """
+        Log robot positions to rerun
+        q: joint positions
+        """
+        transforms = self.robot.fkine_all(q)
+        rr.log(self.name + "/q", rr.Scalars(q))
+        for ix, link in enumerate(self.robot.links):
+            O_T_ix: NDArray[np.float64] = transforms[ix + 1].A
+            rr.log(
+                self.name + "/" + link.name,
+                rr.Transform3D(translation=O_T_ix[:3, 3], mat3x3=O_T_ix[:3, :3]),
+            )
+        pass
+
+    def log_pose(
+        self, position: NDArray, quaternion: NDArray, name: str = "pose"
+    ) -> None:
+        """
+        Log a 3D pose to rerun
+        position: [x, y, z]
+        quaternion: [w, x, y, z]
+        """
+        quad = [quaternion[1], quaternion[2], quaternion[3], quaternion[0]]  # xyzw
         rr.log(
-            name,
+            f"{self.name}/{name}",
             rr.Transform3D(
                 translation=position, rotation=rr.Quaternion(xyzw=quad), axis_length=0.1
             ),
         )
 
-    def load_meshes(self):
-        for ix, link in enumerate(self.robot.links):
+    # def log_poses(self, positions:NDArray, quaternions:NDArray, name: str = "poses") -> None:
+    #     """
+    #     Log multiple 3D poses to rerun
+    #     positions: Nx3 array of [x, y, z]
+    #     quaternions: Nx4 array of [w, x, y, z]
+    #     """
+    #     transforms = []
+    #     for pos, quat in zip(positions, quaternions):
+    #         quad = [quat[1], quat[2], quat[3], quat[0]]
+
+    def log_frame(self, image, camera_name):
+        """
+        Log a camera frame to rerun
+        image: HxWx3 array
+        """
+        rr.log(f"{self.name}/{camera_name}", rr.Image(image).compress())
+
+    def _apply_alpha(self, vertex_colour: NDArray) -> NDArray:
+        if self.alpha is None:
+            return vertex_colour
+        vertex_colour = np.atleast_2d(vertex_colour)
+        vertex_colour[:, -1] = int(self.alpha * 255)
+        breakpoint()
+        return vertex_colour
+
+    def _load_meshes(self) -> None:
+        for link in self.robot.links:
             if len(link.geometry) == 0:
                 continue
             geom = link.geometry[0]
             if isinstance(geom, sg.Mesh):
-                mesh_path = geom.filename
-                mesh = trimesh.load_mesh(mesh_path, process=False)
-                vertex_colour = mesh.visual.to_color().vertex_colors
-
+                mesh = trimesh.load_mesh(geom.filename, process=False)
+                vertex_colour = self._apply_alpha(mesh.visual.to_color().vertex_colors)
                 rr.log(
-                    self.robot_name + "/" + link.name,
+                    f"{self.name}/{link.name}",
                     rr.Mesh3D(
                         vertex_positions=mesh.vertices,
                         triangle_indices=mesh.faces,
@@ -48,97 +90,14 @@ class ReRunRobot:
                     ),
                     static=True,
                 )
-                if self.leader:
-                    vertex_colour_leader = vertex_colour.copy()
-                    vertex_colour_leader = vertex_colour_leader.reshape(-1, 4)
-                    vertex_colour_leader[:, -1] = int(0.5 * 255)
-
-                    rr.log(
-                        self.leader_name + "/" + link.name,
-                        rr.Mesh3D(
-                            vertex_positions=mesh.vertices,
-                            triangle_indices=mesh.faces,
-                            vertex_normals=mesh.vertex_normals,
-                            vertex_colors=vertex_colour_leader,
-                        ),
-                        static=True,
-                    )
-
-    def initialize_video_stream(self, cams: VisionConfig):
-        container = av.open("/dev/null", "w", format="hevc")
-        self.streams = {
-            cam_name: container.add_stream(
-                "libx265", rate=1
-            )  # This needs to be the same obs
-            for cam_name in cams.cameras
-        }
-        for name, stream in self.streams.items():
-            assert isinstance(stream, av.video.stream.VideoStream)
-            stream.width = cams.cameras_params[0].resolution[0]
-            stream.height = cams.cameras_params[0].resolution[1]
-            stream.max_b_frames = 0  # according to rerun docs
-            fake_frame = np.zeros(
-                (
-                    cams.cameras_params[0].resolution[1],
-                    cams.cameras_params[0].resolution[0],
-                    3,
-                ),
-                dtype=np.uint8,
-            )
-            av_frame = av.VideoFrame.from_ndarray(fake_frame, format="rgb24")
-            packet = stream.encode(av_frame)
-            for packet in packet:
-                rr.set_time("time", duration=float(packet.pts * packet.time_base))
-                rr.log(
-                    "video_stream/" + name,
-                    rr.VideoStream(codec=rr.VideoCodec.H265, sample=bytes(packet)),
-                )
-
-    def log_frame(self, frame: NDArray, cam_name: str):
-        """
-        Log a frame of the camera feeds
-        frame [w, h , channels]
-        """
-        av_frame = av.VideoFrame.from_ndarray(frame, format="rgb24")
-        packet = self.streams[cam_name].encode(av_frame)
-        for pkt in packet:
-            rr.set_time("time", duration=float(pkt.pts * pkt.time_base))
-            rr.log(
-                "/video_stream/" + cam_name,
-                rr.VideoStream(codec=rr.VideoCodec.H265, sample=bytes(pkt)),
-            )
-
-    def log_frame_encoded(self, frame: NDArray, cam_name: str):
-        """
-        Alternative approach until the av is working
-        """
-        rr.log("/video_stream/" + cam_name, rr.Image(frame).compress())
-
-    def step_robot(self, q, leader_q: Optional[NDArray] = None):
-        transforms = self.robot.fkine_all(q)
-        rr.log(self.robot_name + "/q", rr.Scalars(q))
-        for ix, link in enumerate(self.robot.links):
-            O_T_ix: NDArray[np.float64] = transforms[ix + 1].A
-            rr.log(
-                self.robot_name + "/" + link.name,
-                rr.Transform3D(translation=O_T_ix[:3, 3], mat3x3=O_T_ix[:3, :3]),
-            )
-        if leader_q is not None:
-            rr.log(self.leader_name + "/q", rr.Scalars(q))
-            transforms = self.robot.fkine_all(leader_q)
-            for ix, link in enumerate(self.robot.links):
-                O_T_ix: NDArray[np.float64] = transforms[ix + 1].A
-                rr.log(
-                    self.leader_name + "/" + link.name,
-                    rr.Transform3D(translation=O_T_ix[:3, 3], mat3x3=O_T_ix[:3, :3]),
-                )
 
 
 if __name__ == "__main__":
     robot = rtb.models.Panda()
     rr.init("urdf_test", recording_id="testing", spawn=True)
     rr.log(f"/{robot.name}", rr.ViewCoordinates.RIGHT_HAND_Z_UP, static=True)
-    rerun_robot = ReRunRobot(robot)
-    rerun_robot.load_meshes()
+    rerun_robot = ReRunRobot(robot, "test")
+    rerun_robot_2 = ReRunRobot(robot, "transparent", alpha=0.1)
     for _ in range(20):
-        rerun_robot.step_robot(rerun_robot.robot.random_q())
+        rerun_robot.log_robot_state(rerun_robot.robot.random_q())
+        rerun_robot_2.log_robot_state(rerun_robot_2.robot.random_q())
